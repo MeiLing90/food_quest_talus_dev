087349abecf783c90f64e4b8f3d8abb6d75ed970
folgerjerome <jerome.folger@talus.ch>
2025-08-23 20:51:28 +0200

    Added voted tracking with pinia

--- web/src/pages/food/FoodSwiper.vue
+++ web/src/pages/food/FoodSwiper.vue
@@ -1,65 +1,52 @@
 <script setup>
-import { ref, computed  } from 'vue';
+import { useFoodStore } from '../../stores/foodStore';
 
-import cardsData from '../../food.json';
-
-const cards = ref(cardsData);
-
-const currentIndex = ref(0);
-
-const currentCard = computed(() => cards.value[currentIndex.value] || null);
-
-function vote(isYes) {
-    if (currentCard.value) {
-        currentCard.value.selected = isYes;
-        currentIndex.value++;
-    }
-}
+const foodStore = useFoodStore();
 </script>
 
 <template>
-    <v-container class="d-flex justify-center">
-        <div v-if="currentCard">
-            <v-card max-width="300">
-                <img
-                    :src="currentCard.image"
-                    class="carouselImage w-100 rounded-md"/>
+  <v-container class="d-flex justify-center">
+    <div v-if="foodStore.currentCard">
+      <v-card max-width="300">
+        <img
+            :src="foodStore.currentCard.image"
+            class="carouselImage w-100 rounded-md"/>
 
-                <v-card-title>{{ currentCard.name }}</v-card-title>
-            </v-card>
+        <v-card-title>{{ foodStore.currentCard.name }}</v-card-title>
+      </v-card>
 
-            <v-row class="justify-center mt-2">
-                <v-btn
-                    color="error"
-                    class="rounded-circle mx-2"
-                    width="60"
-                    height="60"
-                    @click="vote(false)"
-                >
-                    <v-icon>mdi-close</v-icon>
-                </v-btn>
-                <v-btn
-                    color="success"
-                    class="rounded-circle mx-2"
-                    width="60"
-                    height="60"
-                    @click="vote(true)"
-                >
-                    <v-icon>mdi-check</v-icon>
-                </v-btn>
-            </v-row>
+      <v-row class="justify-center mt-2">
+        <v-btn
+            color="error"
+            class="rounded-circle mx-2"
+            width="60"
+            height="60"
+            @click="foodStore.vote(false)"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-btn
+            color="success"
+            class="rounded-circle mx-2"
+            width="60"
+            height="60"
+            @click="foodStore.vote(true)"
+        >
+          <v-icon>mdi-check</v-icon>
+        </v-btn>
+      </v-row>
+    </div>
 
-        </div>
-
-        <div v-else>
-            <p>Keine weiteren Karten</p>
-        </div>
-    </v-container>
+    <div v-else>
+      <p>Keine weiteren Karten</p>
+      <v-btn @click="foodStore.resetStore()">Start Over</v-btn>
+    </div>
+  </v-container>
 </template>
 
 <style scoped>
 .carouselImage {
-    aspect-ratio: 2 / 3;
-    object-fit: cover;
+  aspect-ratio: 2 / 3;
+  object-fit: cover;
 }
 </style>
--- web/src/stores/foodStore.js
+++ web/src/stores/foodStore.js
@@ -3,21 +3,37 @@ import cardsData from '../food.json';
 
 export const useFoodStore = defineStore('food', {
     state: () => ({
-        cards: cardsData,
+        // Map the initial data to ensure a `selected` property is present for consistency
+        allCards: cardsData.map(card => ({ ...card, selected: undefined })),
         currentIndex: 0,
     }),
     getters: {
-        currentCard: (state) => state.cards[state.currentIndex] || null,
-        selectedCards: (state) => state.cards.filter(card => card.selected),
+        // This getter is the core of your filtering logic
+        unvotedCards: (state) => state.allCards.filter(card => card.selected === undefined),
+        currentCard: (state) => {
+            // The currentIndex now correctly points to the index in the unvotedCards array
+            const unvoted = state.allCards.filter(card => card.selected === undefined);
+            return unvoted[state.currentIndex] || null;
+        },
+        selectedCards: (state) => state.allCards.filter(card => card.selected === true),
     },
     actions: {
         vote(isYes) {
-            if (this.currentCard) {
-                this.currentCard.selected = isYes;
-                this.currentIndex++;
+            const cardToUpdate = this.currentCard; // Use the getter to get the current card
+            if (cardToUpdate) {
+                // Find the index of the card in the full `allCards` array
+                const cardIndex = this.allCards.findIndex(card => card.id === cardToUpdate.id);
+                if (cardIndex !== -1) {
+                    // Update the `selected` property of the card in the main array
+                    this.allCards[cardIndex].selected = isYes;
+                }
             }
         },
+        resetStore() {
+            // Reset the 'selected' property on all cards to undefined
+            this.allCards.forEach(card => card.selected = undefined);
+            this.currentIndex = 0;
+        }
     },
-    // Add this line to make the store persistent
     persist: true,
 });
