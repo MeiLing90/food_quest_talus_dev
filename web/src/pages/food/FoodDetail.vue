ceab98033f5d6224eb47e8c8688f582e2d6cc5f9
folgerjerome <jerome.folger@talus.ch>
2025-08-23 21:56:54 +0200

    Added new Foods

--- web/src/pages/food/FoodDetail.vue
+++ web/src/pages/food/FoodDetail.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
-import { useFoodStore } from '../../stores/foodStore'; // Import your Pinia store
+import { useFoodStore } from '../../stores/foodStore';
 import UiParentCard from "../../components/UiParentCard.vue";
 
 const route = useRoute();
@@ -11,14 +10,19 @@ const foodStore = useFoodStore();
 const card = computed(() =>
     foodStore.allCards.find(c => String(c.id) === route.params.id)
 );
-
 </script>
 
 <template>
-
   <UiParentCard :title="card.name" class="maxWidth">
     <img :src="card.image" class="carouselImage w-100 rounded-md" />
-      <p>{{ card.description }}</p>
+    <p>{{ card.description }}</p>
+
+    <hr class="my-4">
+
+    <v-card-text>
+      <h2>Instructions</h2>
+      <div v-html="card.instructions"></div>
+    </v-card-text>
 
   </UiParentCard>
 </template>
