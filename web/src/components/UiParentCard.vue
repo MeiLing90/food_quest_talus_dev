ef11e67dc8c255dce886bb7d653620a5d6c0294f
Dimitri5510 <dimiwoozle@gmail.com>
2025-08-23 17:27:33 +0200

    Create Food List nad swiper

---
+++ web/src/components/UiParentCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String
+});
+</script>
+
+<!-- ===============================|| Ui Parent Card ||=============================== -->
+<template>
+  <v-card
+      elevation="6"
+      class="rounded-xl pa-4 mb-6 mx-auto mt-4"
+      max-width="900"
+  >
+    <v-card-item class="pb-3 ">
+      <div class="d-flex align-center justify-space-between">
+        <slot name="title">
+          <v-card-title class="text-h5 font-weight-medium">
+            {{ title }}
+          </v-card-title>
+        </slot>
+        <slot name="action"></slot>
+      </div>
+    </v-card-item>
+
+    <v-divider class="mb-3"></v-divider>
+
+    <v-card-text class="pt-0">
+      <slot />
+    </v-card-text>
+  </v-card>
+</template>
--- web/src/food/FoodDetail.vue
+++ web/src/food/FoodDetail.vue
@@ -1,29 +1,33 @@
-<script setup>
-import { ref, computed } from 'vue'
-import cardsData from '../food.json';
+<script setup lang="ts">
+import { ref } from 'vue';
 
-const props = defineProps({
-  id: {
-    type: Number,
-    required: true
-  }
-})
+import { computed } from 'vue'
+import cardsData from '../food.json'
+import UiParentCard from "../components/UiParentCard.vue";
+
+// ID aus URL holen
+const urlParams = new URLSearchParams(window.location.search)
+const id = urlParams.get("id") ?? "1"   // fallback = 1
 
 const card = computed(() =>
-    cardsData.find(c => String(c.id) === String(props.id))
+    cardsData.find(c => String(c.id) === id)
 )
+
+
 </script>
 
 <template>
-  <div v-if="card">
-    <p>{{ card.id }}</p>
-    <p>{{ card.name }}</p>
-  </div>
-  <div v-else>
-    <p>Keine Karte gefunden</p>
-  </div>
+
+  <UiParentCard :title="card.name" class="maxWidth">
+    <img :src="card.image" class="carouselImage w-100 rounded-md" />
+      <p>{{ card.description }}</p>
+
+  </UiParentCard>
 </template>
 
 <style scoped>
-
+.carouselImage {
+  aspect-ratio: 32 / 9;
+  object-fit: cover;
+}
 </style>
