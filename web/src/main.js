7da962cda67a0196dfbaea28cf3e74df9aca425a
folgerjerome <jerome.folger@talus.ch>
2025-08-23 18:27:37 +0200

    Added Pinia Persistedstate Storage

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 
 createApp(App).use(vuetify).mount('#app')
