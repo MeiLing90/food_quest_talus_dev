660333bfba04399a2c098ae7ba58b9af8fa06216
Dimitri5510 <dimiwoozle@gmail.com>
2025-08-23 20:27:10 +0200

    Create Food List nad swiper

--- web/src/food/FoodDetail.vue
+++ web/src/food/FoodDetail.vue
@@ -4,6 +4,8 @@ import { ref } from 'vue';
 import { computed } from 'vue'
 import cardsData from '../food.json'
 import UiParentCard from "../components/UiParentCard.vue";
+import Chatbox from "../components/chatbox.vue";
+import Navigation from "../components/navigation.vue";
 
 // ID aus URL holen
 const urlParams = new URLSearchParams(window.location.search)
@@ -20,9 +22,24 @@ const card = computed(() =>
 
   <UiParentCard :title="card.name" class="maxWidth">
     <img :src="card.image" class="carouselImage w-100 rounded-md" />
-      <p>{{ card.description }}</p>
+    <v-container>
+      <v-row>
+        <!-- Linke Seite: Text -->
+        <v-col cols="7">
+          <p>{{ card.description }}</p>
+          <!-- Hier kannst du später weitere Infos einfügen -->
+          <p>{{ card.instructions }}</p>
+          <p>{{ card.ingredients }}</p>
+        </v-col>
 
+        <!-- Rechte Seite: Chatbox -->
+        <v-col cols="5">
+          <chatbox class="mt-0" ref="chatbox" />
+        </v-col>
+      </v-row>
+    </v-container>
   </UiParentCard>
+
 </template>
 
 <style scoped>
